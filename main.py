0399ea38103e637c415ad7de6ddb50646d94ff9e
Iman <hashemi.iman.s@gmail.com>
2026-09-19 12:26:15 -0400

    Improve password generator core logic

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
-import random
 import math
+import secrets
+
 
 alpha = "abcdefghijklmnopqrstuvwxyz"
 num = "0123456789"
 special = "@#$%&*"
+
+
+def generate_pass(length, array, is_alpha=False):
+    characters = []
+
+    for i in range(length):
+        index = secrets.randbelow(len(array))
+        character = array[index]
+
+        if is_alpha:
+            case = secrets.randbelow(2)
+
+            if case == 1:
+                character = character.upper()
+
+        characters.append(character)
+
+    return characters
+
+
+def generate_password(pass_len):
+    alpha_len = pass_len // 2
+    num_len = math.ceil(pass_len * 30 / 100)
+    special_len = pass_len - (alpha_len + num_len)
+
+    password = []
+
+    password += generate_pass(alpha_len, alpha, True)
+    password += generate_pass(num_len, num)
+    password += generate_pass(special_len, special)
+
+    secrets.SystemRandom().shuffle(password)
+
+    return "".join(password)
+
+
 while True:
     try:
         pass_len = int(input("Enter Password Length: "))
@@ -17,27 +54,7 @@ while True:
     except ValueError:
         print("Please enter a valid number.")
 
-alpha_len = pass_len // 2
-num_len = math.ceil(pass_len * 30 / 100)
-special_len = pass_len - (alpha_len + num_len)
 
-password = []
+gen_password = generate_password(pass_len)
 
-def generate_pass(length, array, is_alpha = False):
-    for i in range(length):
-        index = random.randint(0, len(array) - 1)
-        character = array[index]
-        if is_alpha:
-            case = random.randint(0,1)
-            if case == 1:
-                character = character.upper()
-        password.append(character)
-
-generate_pass(alpha_len, alpha, True)
-generate_pass(num_len, num)
-generate_pass(special_len, special)
-
-random.shuffle(password)
-
-gen_password = "".join(password)
-print(gen_password)
+print(f"Generated Password: {gen_password}")
